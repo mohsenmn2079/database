4b17f4c86e96848f00d7ea354160ad7575ee1b7d
nic <mohsenmn2079@gmail.com>
2023-09-02 09:32:03 +0330

    create branch Criteria Queries

--- src/main/java/com/example/databasetest/DatabaseTestApplication.java
+++ src/main/java/com/example/databasetest/DatabaseTestApplication.java
@@ -2,8 +2,10 @@ package com.example.databasetest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class DatabaseTestApplication {
 
     public static void main(String[] args) {
